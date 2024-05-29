1f5034248f39595e0d5fae524bc7b7bd8c5497b5
paul <adriangeorge456@gmail.com>
2024-05-29 19:20:47 +0300

    added icon on tab

--- login.php
+++ login.php
@@ -18,6 +18,7 @@ if ($conn->connect_error) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/icon1.png" type="image/x-icon">
     <title>Login</title>
     <link rel="stylesheet" href="login_style.css">
 </head>
--- signup.php
+++ signup.php
@@ -41,6 +41,7 @@ $conn->close();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/icon1.png" type="image/x-icon">
     <title>Register</title>
     <link rel="stylesheet" href="signup_style.css">
 </head>
