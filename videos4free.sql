9018e9c22c0920dbdf5785e4635be48e3a6e2a1b
PAUL ANDREI VIZITEU <paul_andrei.viziteu@mercedes-benz.com>
2025-05-23 00:54:34 +0300

    added thesis guide, thesis, small tiny update on db

--- videos4free.sql
+++ videos4free.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2024 at 08:15 PM
+-- Generation Time: May 20, 2025 at 09:15 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,20 +32,26 @@ CREATE TABLE `comments` (
   `comment` text NOT NULL,
   `comment_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `username` varchar(50) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `video_id` int(11) DEFAULT NULL,
+  `video_type` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `comment`, `comment_date`, `username`, `user_id`) VALUES
-(18, 'dasds', '2024-05-23 07:31:08', 'testuser', 1),
-(23, 'hi', '2024-05-27 17:52:00', 'paul', 17),
-(45, 'aloha', '2024-05-29 17:32:53', 'beb', 22),
-(46, 'what a video', '2024-05-29 17:33:10', 'beb', 22),
-(47, 'baz', '2024-05-29 18:12:46', 'test', 16),
-(48, 'cookies', '2024-05-29 18:14:17', 'paul', 17);
+INSERT INTO `comments` (`id`, `comment`, `comment_date`, `username`, `user_id`, `video_id`, `video_type`) VALUES
+(18, 'dasds', '2024-05-23 07:31:08', 'testuser', 1, NULL, NULL),
+(23, 'hi', '2024-05-27 17:52:00', 'paul', 17, NULL, NULL),
+(45, 'aloha', '2024-05-29 17:32:53', 'beb', 22, NULL, NULL),
+(46, 'what a video', '2024-05-29 17:33:10', 'beb', 22, NULL, NULL),
+(47, 'baz', '2024-05-29 18:12:46', 'test', 16, NULL, NULL),
+(48, 'cookies', '2024-05-29 18:14:17', 'paul', 17, NULL, NULL),
+(49, 'wowzers', '2025-05-20 18:24:34', 'alexa', 26, 5, 'tv_series'),
+(50, 'yyyyyyy', '2025-05-20 18:24:41', 'alexa', 26, 4, 'tv_series'),
+(51, 'sd', '2025-05-20 19:09:17', 'alexa', 26, 5, 'tv_series'),
+(52, 'asd', '2025-05-20 19:14:09', 'alexa', 26, 6, 'tv_series');
 
 -- --------------------------------------------------------
 
@@ -68,12 +74,12 @@ CREATE TABLE `featured_videos` (
 --
 
 INSERT INTO `featured_videos` (`id`, `title`, `description`, `video_url`, `thumbnail_url`, `category`, `is_featured`) VALUES
-(1, 'Wednesday', 'The ball scene', 'videos/Wednesday_Addams.mp4', 'images/WA1.jpg', 'eeefff', 1),
-(2, 'Peaky Blinders', 'Description for Peaky Blinders video', 'videos/Peaky_Blinders.mp4', 'images/PA1.jpg', 'Drama', 1),
-(3, 'Video 2', 'Description for Video 2', 'videos/video2.mp4', 'images/V2.jpg', 'Miscellaneouss', 1),
-(4, 'Breaking Bad', 'Description for Breaking Bad video', 'videos/Breaking_Bad.mp4', 'images/BB1.jpg', 'Drama', 1),
-(5, 'Bojack Horseman', 'bb', 'videos/Bojack_Horseman.mp4', 'images/BH1.jpg', 'testjava', 1),
-(6, 'Infinity War', 'Description for Infinity War video', 'videos/Infinity_War.mp4', 'images/IW1.jpg', 'Action', 1);
+(1, 'First video', 'City', 'videos/first.mp4', 'images/first.jpg', 'thesis', 1),
+(2, 'Clock', 'Clock video', 'videos/second.mp4', 'images/second.jpg', 'Thesis', 1),
+(3, 'third', 'video', 'videos/third.mp4', 'images/third.jpg', 'thesis', 1),
+(4, 'fourth', 'desc', 'videos/fourth.mp4', 'images/fourth.jpg', 'thesis', 1),
+(5, 'Fifth', 'bb', 'videos/fifth.mp4', 'images/fifth.jpg', 'thesis', 1),
+(6, 'Sixth', 'Description', 'videos/sixth.mp4', 'images/sixth.jpg', 'thesis', 1);
 
 -- --------------------------------------------------------
 
@@ -89,20 +95,21 @@ CREATE TABLE `movies` (
   `director` varchar(255) DEFAULT NULL,
   `genre` varchar(100) DEFAULT NULL,
   `rating` decimal(3,1) DEFAULT NULL,
-  `thumbnail_url` varchar(255) DEFAULT NULL
+  `thumbnail_url` varchar(255) DEFAULT NULL,
+  `video_url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `movies`
 --
 
-INSERT INTO `movies` (`id`, `title`, `description`, `release_year`, `director`, `genre`, `rating`, `thumbnail_url`) VALUES
-(1, 'Spider-Man', 'incroyable', 2022, 'bob', 'Action', 5.0, 'images/spider.jpg'),
-(2, 'Harry Potter', 'youre a lizard', 2323, 'beb', 'ef', 4.0, 'images/harry.jpg'),
-(3, 'Avengers: Infinity War', 'asdads', 232131, 'dasdadsd', 'dfsdsf', 6.0, 'images/infinity.jpg'),
-(4, 'Mario', NULL, NULL, NULL, NULL, NULL, 'images/mario.jpg'),
-(5, 'Hunger Games', NULL, NULL, NULL, NULL, NULL, 'images/hunger.jpg'),
-(6, 'Lion King', NULL, NULL, NULL, NULL, NULL, 'images/lion.jpg');
+INSERT INTO `movies` (`id`, `title`, `description`, `release_year`, `director`, `genre`, `rating`, `thumbnail_url`, `video_url`) VALUES
+(1, 'First Movie', 'I hate copyright', 2022, 'Bob', 'Action', 5.0, 'images/movie1.jpg', 'videos/firstmovie.mp4'),
+(2, 'Movie 2', 'I hate copyright', 1999, 'Alexa', 'any', 4.0, 'images/movie2.jpg', 'videos/secondmovie.mp4'),
+(3, 'Movie 3', 'I hate copyright', 2014, 'Alexa', 'any', 6.0, 'images/movie3.jpg', 'videos/thirdmovie.mp4'),
+(4, 'Movie 4', 'I hate copyright', 2015, 'Bob', 'any', 9.0, 'images/movie4.jpg', 'videos/fourthmovie.mp4'),
+(5, 'Movie 5', 'I hate copyright', 2016, 'Joe', 'any', 7.2, 'images/movie5.jpg', 'videos/fifthmovie.mp4'),
+(6, 'Movie 6', 'I hate copyright', 2020, 'Steve', 'any', 5.6, 'images/movie6.jpg', 'videos/sixthmovie.mp4');
 
 -- --------------------------------------------------------
 
@@ -135,20 +142,21 @@ CREATE TABLE `tv_series` (
   `description` text DEFAULT NULL,
   `creator` varchar(255) DEFAULT NULL,
   `genre` varchar(100) DEFAULT NULL,
-  `thumbnail_url` varchar(255) DEFAULT NULL
+  `thumbnail_url` varchar(255) DEFAULT NULL,
+  `video_url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tv_series`
 --
 
-INSERT INTO `tv_series` (`id`, `title`, `description`, `creator`, `genre`, `thumbnail_url`) VALUES
-(1, 'Sheldon', 'young sheldoner', 'bober', 'slice of lifere', 'images/sheldon.jpg'),
-(2, 'The Witcher', 'Description for The Witcher TV series', 'Creators', 'Fantasy', 'images/witcher.jpg'),
-(3, 'NCIS', 'Description for NCIS TV series', 'Creators', 'Crime', 'images/ncis.jpg'),
-(4, 'Rick and Morty', 'Description for Rick and Morty TV seriese', 'Creators', 'Animation', 'images/rick.jpg'),
-(5, 'Brooklyn Nine-Nine', 'Description for Brooklyn Nine-Nine TV series', 'Creators', 'Comedy', 'images/b99.jpg'),
-(6, 'The Boys', 'Description for The Boys TV series', 'Creators', 'Action', 'images/boys.jpg');
+INSERT INTO `tv_series` (`id`, `title`, `description`, `creator`, `genre`, `thumbnail_url`, `video_url`) VALUES
+(1, 'Tv 1', 'I HATE COPYRIGHT', 'Alexa', 'any', 'images/tv1.jpg', 'videos/firsttv.mp4'),
+(2, 'Tv 2', 'I HATE COPYRIGHT', 'Janet', 'any', 'images/tv2.jpg', 'videos/secondtv.mp4'),
+(3, 'Tv 3', 'I HATE COPYRIGHT', 'Alexa', 'Crime', 'images/tv3.jpg', 'videos/thirdtv.mp4'),
+(4, 'Tv 4', 'I HATE COPYRIGHT', 'Kayle', 'Animation', 'images/tv4.jpg', 'videos/fourthtv.mp4'),
+(5, 'Tv 5', 'I HATE COPYRIGHT', 'George', 'Comedy', 'images/tv5.jpg', 'videos/fifthtv.mp4'),
+(6, 'Tv 6', 'I HATE COPYRIGHT', 'Ian', 'Action', 'images/tv6.jpg', 'videos/sixthtv.mp4');
 
 -- --------------------------------------------------------
 
@@ -171,7 +179,9 @@ INSERT INTO `users` (`id`, `username`, `password`, `registration_date`) VALUES
 (1, 'testuser', 'testpassword', '2024-05-02 18:48:07'),
 (16, 'test', 'test', '2024-05-27 14:51:42'),
 (17, 'paul', 'parola', '2024-05-27 17:41:25'),
-(22, 'beb', 'beb', '2024-05-29 17:32:47');
+(22, 'beb', 'beb', '2024-05-29 17:32:47'),
+(25, 'boba', '$2y$10$Hhe7naF8rjzxl0UEXVz7N.f.yfSLXegBkYECRtQ0/nGLUcczRb3Ga', '2025-05-20 16:50:22'),
+(26, 'alexa', '$2y$10$0ZVlPT0aO4ZIUYdVzIopkOdMTtF7jOl9PFJfLobdgVXNfJ4ZZ7Xpy', '2025-05-20 16:56:32');
 
 --
 -- Indexes for dumped tables
@@ -224,7 +234,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `featured_videos`
@@ -254,7 +264,7 @@ ALTER TABLE `tv_series`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
